84ac5a15d36c4aa6d5eeb71fd5460c5d74c1d62b
yj <1638519865@qq.com>
2023-02-24 12:40:55 +0800

    加入opencv使用部分

---
+++ ch03/coordinateTransform/cmake-build-debug/CMakeFiles/coordinateTransform.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/coordinateTransform.dir/coordinateTransform.cpp.o"
+  "CMakeFiles/coordinateTransform.dir/coordinateTransform.cpp.o.d"
+  "coordinateTransform"
+  "coordinateTransform.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/coordinateTransform.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
