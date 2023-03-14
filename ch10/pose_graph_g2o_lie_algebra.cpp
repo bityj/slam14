7a66e1003029e68af576edef8a0647f3cacb8ced
yj <1638519865@qq.com>
2023-03-14 10:53:24 +0800

    加入李代数上的位姿图优化

--- ch10/pose_graph_g2o_lie_algebra.cpp
+++ ch10/pose_graph_g2o_lie_algebra.cpp
@@ -1,78 +1,204 @@
-#include <iostream>
-#include <fstream>
-#include <string>
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<Eigen/Core>
 
-#include <g2o/types/slam3d/types_slam3d.h>
-#include <g2o/core/block_solver.h>
-#include <g2o/core/optimization_algorithm_levenberg.h>
-#include <g2o/solvers/eigen/linear_solver_eigen.h>
+#include<g2o/core/base_vertex.h>
+#include<g2o/core/base_binary_edge.h>
+#include<g2o/core/block_solver.h>
+#include<g2o/core/optimization_algorithm_levenberg.h>
+#include<g2o/solvers/eigen/linear_solver_eigen.h>
+
+#include<sophus/se3.hpp>
 
 using namespace std;
+using namespace Eigen;
+using Sophus::SE3d;
+using Sophus::SO3d;
 
-/************************************************
- * 本程序演示如何用g2o solver进行位姿图优化
- * sphere.g2o是人工生成的一个Pose graph，我们来优化它。
- * 尽管可以直接通过load函数读取整个图，但我们还是自己来实现读取代码，以期获得更深刻的理解
- * 这里使用g2o/types/slam3d/中的SE3表示位姿，它实质上是四元数而非李代数.
- * **********************************************/
+typedef Matrix<double,6, 6> Matrix6d;
 
-int main(int argc, char **argv) {
-    if (argc != 2) {
-        cout << "Usage: pose_graph_g2o_SE3 sphere.g2o" << endl;
+Matrix6d JRInv(const SE3d &e){
+    Matrix6d J;
+    J.block(0, 0, 3, 3) = SO3d::hat(e.so3().log());
+    J.block(0, 3, 3, 3) = SO3d::hat(e.translation());
+    J.block(3, 0, 3, 3) = Matrix3d::Zero(3, 3);
+    J.block(3, 3, 3, 3) = SO3d::hat(e.so3().log());
+    J = Matrix6d::Identity();
+    return J;
+}
+
+typedef Matrix<double, 6, 1> Vector6d;
+
+class VertexSE3LieAlgebra : public g2o::BaseVertex<6, SE3d> {
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+    virtual bool read(istream &is) override{
+        double data[7];
+        for(int i = 0; i < 7; i++)
+            is >> data[i];
+        setEstimate(SE3d(
+                Quaterniond(data[6], data[3], data[4], data[5]),
+                Vector3d(data[0], data[1], data[2])
+        ));
+        return true;
+    }
+
+    virtual bool write(ostream &os) const override{
+        os << id() << " ";
+        Quaterniond q = _estimate.unit_quaternion();
+        os << _estimate.translation().transpose() << " ";
+        os << q.coeffs()[0] << " " << q.coeffs()[1] << " " << q.coeffs()[2] << " " << q.coeffs()[3] << endl;
+        return true;
+    }
+
+    virtual void setToOriginImpl() override{
+        _estimate = SE3d();
+    }
+
+    virtual void oplusImpl(const double *update) override {
+        Vector6d upd;
+        upd << update[0], update[1], update[2], update[3], update[4], update[5];
+        _estimate = SE3d::exp(upd) * _estimate;
+    }
+};
+
+class EdgeSE3LieAlgebra : public g2o::BaseBinaryEdge<6, SE3d, VertexSE3LieAlgebra, VertexSE3LieAlgebra>{
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+    virtual bool read(istream &is) override{
+        double data[7];
+        for(int i = 0; i < 7; i++)
+            is >> data[i];
+        Quaterniond q(data[6], data[3], data[4], data[5]);
+        q.normalize();
+        setMeasurement(SE3d(q, Vector3d(data[0], data[1], data[2])));
+        for(int i = 0; i < information().rows() && is.good(); i++)
+            for(int j = i; j < information().cols() && is.good(); j++){
+                is >> information()(i, j);
+                if(i != j)
+                    information()(j, i) = information()(i, j);
+            }
+        return true;
+    }
+
+    virtual bool write(ostream &os) const override {
+        VertexSE3LieAlgebra *v1 = static_cast<VertexSE3LieAlgebra *> (_vertices[0]);
+        VertexSE3LieAlgebra *v2 = static_cast<VertexSE3LieAlgebra *> (_vertices[1]);
+        os << v1->id() << " " << v2->id() << " ";
+        SE3d m = _measurement;
+        Eigen::Quaterniond q = m.unit_quaternion();
+        os << m.translation().transpose() << " ";
+        os << q.coeffs()[0] << " " << q.coeffs()[1] << " " << q.coeffs()[2] << " " << q.coeffs()[3] << " ";
+
+        for(int i = 0; i << information().rows(); i++)
+            for(int j = i; j < information().cols(); j++){
+                os << information()(i, j) << " ";
+            }
+        os << endl;
+        return true;
+    }
+
+    virtual void computeError() override {
+        SE3d v1 = (static_cast<VertexSE3LieAlgebra *> (_vertices[0]))->estimate();
+        SE3d v2 = (static_cast<VertexSE3LieAlgebra *> (_vertices[1]))->estimate();
+        _error = (_measurement.inverse() * v1.inverse() * v2).log();
+    }
+
+    virtual void linearizeOplus() override{
+        SE3d v1 = (static_cast<VertexSE3LieAlgebra *> (_vertices[0]))->estimate();
+        SE3d v2 = (static_cast<VertexSE3LieAlgebra *> (_vertices[1]))->estimate();
+        Matrix6d J = JRInv(SE3d::exp(_error));
+        _jacobianOplusXi = -J * v2.inverse().Adj();
+        _jacobianOplusXj = J * v2.inverse().Adj();
+    }
+};
+
+int main(int argc, char **argv){
+    if(argc != 2){
+        cout << "Usage: pose_graph_g2o_SE3_lie shere.g2o" << endl;
         return 1;
     }
     ifstream fin(argv[1]);
-    if (!fin) {
+    if(!fin){
         cout << "file " << argv[1] << " does not exist." << endl;
         return 1;
     }
 
-    // 设定g2o
     typedef g2o::BlockSolver<g2o::BlockSolverTraits<6, 6>> BlockSolverType;
     typedef g2o::LinearSolverEigen<BlockSolverType::PoseMatrixType> LinearSolverType;
     auto solver = new g2o::OptimizationAlgorithmLevenberg(
             g2o::make_unique<BlockSolverType>(g2o::make_unique<LinearSolverType>()));
-    g2o::SparseOptimizer optimizer;     // 图模型
-    optimizer.setAlgorithm(solver);   // 设置求解器
-    optimizer.setVerbose(true);       // 打开调试输出
+    g2o::SparseOptimizer optimizer;
+    optimizer.setAlgorithm(solver);
+    optimizer.setVerbose(true);
 
-    int vertexCnt = 0, edgeCnt = 0; // 顶点和边的数量
-    while (!fin.eof()) {
+    int vertexCnt = 0, edgeCnt = 0;
+
+    vector<VertexSE3LieAlgebra *> vertices;
+    vector<EdgeSE3LieAlgebra *> edges;
+    while(!fin.eof()){
         string name;
         fin >> name;
-        if (name == "VERTEX_SE3:QUAT") {
-            // SE3 顶点
-            g2o::VertexSE3 *v = new g2o::VertexSE3();
+        if(name == "VERTEX_SE3:QUAT"){
+            VertexSE3LieAlgebra *v = new VertexSE3LieAlgebra();
             int index = 0;
             fin >> index;
             v->setId(index);
             v->read(fin);
             optimizer.addVertex(v);
             vertexCnt++;
-            if (index == 0)
+            vertices.push_back(v);
+            if(index == 0)
                 v->setFixed(true);
-        } else if (name == "EDGE_SE3:QUAT") {
-            // SE3-SE3 边
-            g2o::EdgeSE3 *e = new g2o::EdgeSE3();
-            int idx1, idx2;     // 关联的两个顶点
+        }else if(name == "EDGE_SE3:QUAT"){
+            EdgeSE3LieAlgebra *e = new EdgeSE3LieAlgebra();
+            int idx1, idx2;
             fin >> idx1 >> idx2;
             e->setId(edgeCnt++);
             e->setVertex(0, optimizer.vertices()[idx1]);
             e->setVertex(1, optimizer.vertices()[idx2]);
             e->read(fin);
             optimizer.addEdge(e);
+            edges.push_back(e);
         }
-        if (!fin.good()) break;
+        if(!fin.good()) break;
     }
 
-    cout << "read total " << vertexCnt << " vertices, " << edgeCnt << " edges." << endl;
+    cout << "read total " << vertexCnt << " vertices, " << edgeCnt << " edges."  << endl;
 
     cout << "optimizing ..." << endl;
     optimizer.initializeOptimization();
     optimizer.optimize(30);
 
     cout << "saving optimization results ..." << endl;
-    optimizer.save("result.g2o");
 
+    ofstream fout("result_lie.g2o");
+    for(VertexSE3LieAlgebra *v:vertices){
+        fout << "VERTEX_SE3:QUAT ";
+        v->write(fout);
+    }
+    for(EdgeSE3LieAlgebra *e:edges){
+        fout << "EDGE_SE3:QUAT ";
+        e->write(fout);
+    }
+    fout.close();
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
